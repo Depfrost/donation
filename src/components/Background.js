af566a9fb7dbb8c707dcbeca0de9663a0a2aad44
DavidG <davidgrondinf@gmail.com>
2020-10-29 05:45:02 +0400

    Added more backgrounds

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,5 +1,7 @@
 const backgroundImages = [
   "https://animatedandy.com/wp-content/uploads/2019/07/Clannad_.After_.Story_.full_.2061869.jpg",
+  "https://www.wallpaperup.com/uploads/wallpapers/2016/11/11/1039718/db7b51861a05b686efd495c2df35edc5-700.jpg",
+  "https://animatedandy.com/wp-content/uploads/2019/07/Clannad_.After_.Story_.full_.2061869.jpg",
   "https://www.wallpaperup.com/uploads/wallpapers/2016/11/11/1039718/db7b51861a05b686efd495c2df35edc5-700.jpg"
 ];
 
